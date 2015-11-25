5e818b2f2cc262602290990a62fc762d8eab23a8
Your Name <you@example.com>
2015-11-25 21:59:55 +0900

    HTTPS

--- JavaScript/Dash/DashContents_SNS.js
+++ JavaScript/Dash/DashContents_SNS.js
@@ -27,49 +27,49 @@ function setupSNSLens_SharePost(){
     
     {
       title:'記事をはてなブックマークに追加',
-      link:"//b.hatena.ne.jp/add?mode=confirm&url=" + gPostInfo.canonicalUrl + "&title=" + postTitle,
+      link:"//b.hatena.ne.jp/add?mode=confirm&url=" + gPostInfo.url + "&title=" + postTitle,
       iconURL:'//lh3.googleusercontent.com/-7M3z08cysmU/VRolAQl79OI/AAAAAAABAQU/AwmWm4pcecY/s800/HatenaButton_48.jpg',
     },
     
     {
       title:'記事をツイッターで共有',
-      link:"//twitter.com/share?url=" + gPostInfo.canonicalUrl + "&text="  + postTitle + "&hashtags=kledgeb",
+      link:"//twitter.com/share?url=" + gPostInfo.url + "&text="  + postTitle + "&hashtags=kledgeb",
       iconURL:'//lh5.googleusercontent.com/-NpqimLJuM8k/VRolBlZAFsI/AAAAAAABAQI/3lH9wOOFmR4/s800/TweetButton_48.jpg',
     },
 
     {
       title:'記事をfacebookで共有',
-      link:"//www.facebook.com/sharer.php?u=" + gPostInfo.canonicalUrl + "&t="  + postTitle,
+      link:"//www.facebook.com/sharer.php?u=" + gPostInfo.url + "&t="  + postTitle,
       iconURL:'//lh6.googleusercontent.com/-H3rMXXmz3hY/VRoo2JTbh-I/AAAAAAABAQo/MVRvaVya3Ds/s800/facebookButton_48.jpg',
     },
 
     {
       title:'記事をGoogle+で共有',
-      link:"//plus.google.com/share?url=" + gPostInfo.canonicalUrl,
+      link:"//plus.google.com/share?url=" + gPostInfo.url,
       iconURL:'//lh6.googleusercontent.com/-qU0vsgkUrig/VRok_u29a9I/AAAAAAABAPc/ptVAk184T9I/s800/GooglePlusButton_48.jpg',
     },
 
     {
       title:'記事をTumblrで共有',
-      link:"//www.tumblr.com/share/link?url=" + gPostInfo.canonicalUrl + "&name="  + postTitle,
+      link:"//www.tumblr.com/share/link?url=" + gPostInfo.url + "&name="  + postTitle,
       iconURL:'//lh5.googleusercontent.com/-OiKGTQgqiJ8/VRolBLL6FlI/AAAAAAABAP0/o-fxjbZrdts/s800/TumblrButton_48.jpg',
     },
     
     {
       title:'記事をPocketに保存',
-      link:"//getpocket.com/edit?url=" + gPostInfo.canonicalUrl + "&title="  + postTitle,
+      link:"//getpocket.com/edit?url=" + gPostInfo.url + "&title="  + postTitle,
       iconURL:'//lh4.googleusercontent.com/-BRhOHd48HQ0/VRok_uqDAGI/AAAAAAABAQQ/3rfd8nNHXiA/s800/GetPocketButton_48.jpg',
     },
 
     {
       title:'記事をLinkedinで共有',
-      link:"//www.linkedin.com/shareArticle?mini=true&url=" + gPostInfo.canonicalUrl + "&title="  + postTitle,
+      link:"//www.linkedin.com/shareArticle?mini=true&url=" + gPostInfo.url + "&title="  + postTitle,
       iconURL:'//lh4.googleusercontent.com/-4W5qWmMr9Pg/VRolAne3NxI/AAAAAAABAQE/rMOr8CJ4f9E/s800/LinkedinButton_48.jpg',
     },
     
     {
       title:'Feedlyで購読',
-      link:'//cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Ffeeds.feedburner.com%2FKledgeb',
+      link:'//cloud.feedly.com/#subscription%2Ffeed%2Fhttps%3A%2F%2Ffeeds.feedburner.com%2FKledgeb',
       iconURL:'//lh5.googleusercontent.com/-A-iS8Y1ldXY/VRok_nQceLI/AAAAAAABAPk/8fOk3S86kfA/s800/FeedlyButton_48.jpg',
     },
   ];
